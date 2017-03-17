40e118211b76b25fb8de04f903945a592ae0b603
Alex Zanevskyy <alex.zanevskyy@aspose.com>
2017-03-17 15:40:06 +0200

    synchronize data

--- RemoteMarket/App_Start/RouteConfig.cs
+++ RemoteMarket/App_Start/RouteConfig.cs
@@ -16,8 +16,10 @@ namespace RemoteMarket
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "WorkType", action = "Index", id = UrlParameter.Optional }
             );
+
+          
         }
     }
 } 
